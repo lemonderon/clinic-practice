170190473c253a991b2717a2cc9beb191462c193
Yaroslav Kuchmenko <yaroslav.kuchmenko@protonmail.com>
2026-09-22 14:28:13 +0300

    Rename Django configuration package and standardize line endings

--- config/asgi.py
+++ config/asgi.py
@@ -1,7 +1,9 @@
-"""ASGI config for mysite project.
+"""
+ASGI config for config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
+For more information on this file, see
 https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 """
 
@@ -9,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
