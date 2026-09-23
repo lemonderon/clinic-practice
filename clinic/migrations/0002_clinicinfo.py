953abab8fd96a831790fb54bc2d37cdd0c2cff96
Yaroslav Kuchmenko <yaroslav.kuchmenko@protonmail.com>
2026-09-23 08:48:52 +0300

    Add branch, service, and clinic information migrations
    Inspect 0001_initial and 0002_clinicinfo, then run python manage.py migrate. These preserve the existing migration identities; a new student project can generate all three models in one initial migration. SQLite remains local and untracked.

---
+++ clinic/migrations/0002_clinicinfo.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('clinic', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ClinicInfo',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=160)),
+                ('description', models.TextField()),
+                ('contacts', models.TextField()),
+            ],
+        ),
+    ]
