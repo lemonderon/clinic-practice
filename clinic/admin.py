8f7d5492ba307b712c50f0463719e039980b2352
Yaroslav Kuchmenko <yaroslav.kuchmenko@protonmail.com>
2026-09-23 08:48:52 +0300

    Customize clinic admin branding and model lists
    Configure one ModelAdmin registration per model. Display branch name/manager; service code/name/coordinator/contact/branch; ClinicInfo id/name/contacts. Explain that the homepage selects the lowest ClinicInfo ID.

--- clinic/admin.py
+++ clinic/admin.py
@@ -2,6 +2,40 @@ from django.contrib import admin
 
 from .models import Branch, ClinicInfo, Service
 
-admin.site.register(Branch)
-admin.site.register(Service)
-admin.site.register(ClinicInfo)
+admin.site.site_header = "Адміністрування ветеринарної клініки «Північна зірка»"
+admin.site.site_title = "Адмінпанель «Північна зірка»"
+admin.site.index_title = "Керування клінікою"
+
+
+@admin.register(ClinicInfo)
+class ClinicInfoAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "contacts")
+    ordering = ("id",)
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("name", "description", "contacts"),
+                "description": (
+                    "На головній сторінці відображається запис з інформацією "
+                    "про клініку з найменшим ідентифікатором."
+                ),
+            },
+        ),
+    )
+
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ("name", "manager_name")
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        "code",
+        "name",
+        "coordinator_name",
+        "coordinator_contact",
+        "branch",
+    )
